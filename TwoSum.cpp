2f32841efdebc7a4ae226690db2e201ffdca4304
neelSchool <144788333+neelSchool@users.noreply.github.com>
2024-08-12 22:54:27 +0100

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i <= nums.size(); i++)
+        {
+            for (int j = 0; j != i; j++)
+            {
+                 int sum = nums[i] + nums[j];
+                 if (sum == target)
+                 {
+                    return {i,j};
+                 }
+            }
+        }
+        return {};
+    }
+};
