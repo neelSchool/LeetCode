dfbe1ba3993fb65696fa30e75131f3695a2f4917
neelSchool <144788333+neelSchool@users.noreply.github.com>
2024-08-15 22:28:29 +0100

    Create ValidParentheses.cpp

---
+++ ValidParentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stack.push(s[i]);
+            } else {
+                if (stack.empty() || 
+                    (s[i] == ')' && stack.top() != '(') ||
+                    (s[i] == '}' && stack.top() != '{') ||
+                    (s[i] == ']' && stack.top() != '[')) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+};
